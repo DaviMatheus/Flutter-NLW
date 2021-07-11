3c564e1c3d907b15dafe7944aa441ddd7fee0332
DaviMatheus <matdavimat@gmail.com>
2021-07-10 23:26:57 -0300

    adding native camera into app

--- payflown/lib/modules/home/homepage.dart
+++ payflown/lib/modules/home/homepage.dart
@@ -1,21 +1,22 @@
-
 import 'package:flutter/material.dart';
 import 'package:payflown/modules/home/home_controller.dart';
 import 'package:payflown/shared/themes/app_colors.dart';
 import 'package:payflown/shared/themes/app_text_styles.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  final homeController = HomeController();
+  final controller = HomeController();
   final pages = [
-    Container(color: Colors.red,),
-    Container(color: Colors.blue,)
+    Container(
+      color: Colors.red,
+    ),
+    Container(color: Colors.blue),
   ];
   @override
   Widget build(BuildContext context) {
@@ -28,49 +29,70 @@ class _HomePageState extends State<HomePage> {
           child: Center(
             child: ListTile(
               title: Text.rich(
-                TextSpan(text: "Olá, ",style: TextStyles.titleRegular,
-                children: [
-                  TextSpan(text: "Davi", style: TextStyles.titleBoldBackground)
-                ]),
+                TextSpan(
+                    text: "Olá, ",
+                    style: TextStyles.titleRegular,
+                    children: [
+                      TextSpan(
+                          text: "Gabul", style: TextStyles.titleBoldBackground)
+                    ]),
               ),
-              subtitle: Text("Mantenha suas contas em dia",style: TextStyles.captionShape,),
+              subtitle: Text(
+                "Mantenha suas contas em dia",
+                style: TextStyles.captionShape,
+              ),
               trailing: Container(
-                width: 48,
                 height: 48,
-                decoration: BoxDecoration(color: Colors.black,
-                borderRadius: BorderRadius.circular(5))
+                width: 48,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5)),
               ),
             ),
           ),
         ),
-        ),
-      body: pages[homeController.currentPage],
+      ),
+      body: pages[controller.currentPage],
       bottomNavigationBar: Container(
         height: 90,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-          IconButton(onPressed: (){
-            homeController.setPage(0);
-            setState(() {});
-          }, icon: Icon(Icons.home, color: AppColors.primary,)),
-          GestureDetector(
-            onTap: (){
-              print("Clicou");
-            },
-            child: Container(
-              width: 56,
-              height: 56,
-              decoration: BoxDecoration(color: AppColors.primary,
-              borderRadius: BorderRadius.circular(5)),
-              child:IconButton(onPressed: (){}, icon: Icon(Icons.add_box_outlined, color: AppColors.background,)),  
+            IconButton(
+                onPressed: () {
+                  controller.setPage(0);
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.home,
+                  color: AppColors.primary,
+                )),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, "/barcode_scanner");
+              },
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(5)),
+                child: Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.background,
+                ),
+              ),
             ),
-          ),
-          IconButton(onPressed: (){
-            homeController.setPage(1);
-            setState(() {});
-          }, icon: Icon(Icons.description_outlined, color: AppColors.body,)),
-        ],
+            IconButton(
+                onPressed: () {
+                  controller.setPage(1);
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.description_outlined,
+                  color: AppColors.body,
+                ))
+          ],
         ),
       ),
     );
